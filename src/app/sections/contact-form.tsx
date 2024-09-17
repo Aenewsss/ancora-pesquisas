fa6cf882d1bfa26f1efc93e2737ea2ad97feccfb
Aenewsss <aenamartinelli@gmail.com>
2024-09-16 23:18:23 -0300

    fix: changes filipe

--- src/app/sections/contact-form.tsx
+++ src/app/sections/contact-form.tsx
@@ -1,30 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ContactForm() {
     return (
         <section id="contact" className="py-20 relative">
             <Image unoptimized className="object-cover absolute top-0 left-0 z-0 w-full h-full" src="/header.png" width={300} height={300} alt="Hero" />
             <div className="container mx-auto relative z-10 flex justify-center">
-                <div className="flex md:gap-44 gap-10 flex-wrap">
+                <div className="flex md:gap-44 gap-5 flex-wrap justify-center">
                     <div className="flex flex-col gap-8">
                         <div className="flex flex-col gap-3 md:text-start text-center">
                             <h2 className="text-3xl">Descubra o potencial da Âncora!</h2>
                             <p>Entre em contato agora mesmo</p>
                         </div>
                         <div className="flex flex-wrap gap-4 items-center justify-center">
-                            <div className="h-40 flex flex-col gap-1 justify-center items-center bg-white p-8 rounded-xl">
+                            <Link target="_blank" href="https://api.whatsapp.com/send?phone=5571999460468&text=Olá, venho através do site e gostaria de mais informações!" className="h-40 flex flex-col gap-1 justify-center items-center bg-white p-8  min-w-[300px] rounded-xl">
                                 <Image src="/icons/phone.svg" width={40} height={40} alt="Telefone" />
                                 <h3 className="text-xl font-bold text-primary">WhatsApp</h3>
-                                <p className="text-sm text-black">+55 71 9924-5822</p>
-                            </div>
-                            <div className="h-40 flex flex-col gap-1 justify-center items-center bg-white p-8 rounded-xl">
+                                <p className="text-sm text-black">+55 71 9 9946-0468</p>
+
+                            </Link>
+                            <Link target="_blank" href="mailto:ancora.pesquisas06@hotmail.com" className="h-40 flex flex-col gap-1 justify-center items-center bg-white p-8  min-w-[300px] rounded-xl">
                                 <Image src="/icons/mail.svg" width={40} height={40} alt="Telefone" />
                                 <h3 className="text-xl font-bold text-primary">Email</h3>
-                                <a href="mailto:elizangelaribeiro.three@hotmail.com" className="text-sm text-black">elizangelaribeiro.three@hotmail.com</a>
-                            </div>
+                                <p className="text-sm text-black">ancora.pesquisas06@hotmail.com</p>
+                            </Link>
                         </div>
                     </div>
-                    <form className="bg-white p-6 rounded text-primary flex flex-col">
+                    <form className="bg-white p-6 rounded text-primary flex flex-col min-w-[200px]">
                         <h3 className="text-xl font-bold">Entre em contato agora mesmo!</h3>
                         <input type="text" placeholder="Nome" className="w-full p-2 mt-2 bg-gray-300 placeholder:text-slate-400 rounded-full px-4 py-2" />
                         <input type="text" placeholder="Telefone" className="w-full p-2 mt-2 bg-gray-300 placeholder:text-slate-400 rounded-full px-4 py-2" />
--- src/app/sections/services.tsx
+++ src/app/sections/services.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Services() {
     return (
-        <section id="services" className="py-20">
+        <section id="services" className="py-20 md:px-0 px-2">
             <h2 className="text-3xl font-bold text-center mb-12 text-yellow">Nossos Serviços</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-2 gap-6">
                 <div className="flex justify-center relative">
                     <Image unoptimized className="w-full" src="/services/searches.png" width={300} height={300} alt="Serviço" />
                     <h3 className="tracking-wide text-center uppercase absolute bottom-6 text-xl font-medium mt-4">Pesquisa Quantitativas<br />e Qualitativas</h3>
